4a2c0e5f29bdc2387798f463bec44c1ebc064410
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-02-08 20:20:27 -0300

    fazendo importaçao inss funcionar - funcionando, falta tratar erros

--- vpn-fhemig/app/controllers/contracheque_importations_controller.rb
+++ vpn-fhemig/app/controllers/contracheque_importations_controller.rb
@@ -43,27 +43,40 @@ class ContrachequeImportationsController < ApplicationController
     options.add_argument('--disable-popup-blocking')
     options.add_argument('--disable-translate')
     options.add_argument("--incognito")
-    driver = Selenium::WebDriver.for :chrome, options: options
-    driver.navigate.to 'http://sal.receita.fazenda.gov.br/PortalSalInternet/faces/pages/calcContribuicoesCI/filiadosApos/selecionarOpcoesCalculoApos.xhtml'
-    # http://3qilabs.com/how_to/how-to-zoom-in-zoom-out-using-selenium-webdriver/
-    driver.execute_script "document.body.style['-webkit-transform'] = \"scale(2.5)\";"
-    # https://snippets.aktagon.com/snippets/505-how-to-capture-screenshots-with-selenium-ruby-and-firefox
-    driver.save_screenshot('screenshot.png')
-    recapcha = recapcha(driver.save_screenshot('screenshot.png'))
-    driver.execute_script "document.body.style['-webkit-transform'] = \"scale(1)\";"
-    driver.find_element(id: "captcha_campo_resposta").send_keys recapcha.text
-    driver.find_element(id: "opcoesCalcContribuicoesCI:nome").click()
-    driver.find_element(id: "opcoesCalcContribuicoesCI:nome").send_keys '106.05052.503'
-    driver.find_element(id: "opcoesCalcContribuicoesCI:botaoConfirmar").click()
+    time_out = Selenium::WebDriver::Remote::Http::Default.new
+    time_out.read_timeout = 120 # seconds
+    driver = Selenium::WebDriver.for :chrome, options: options, http_client: time_out
+    recapcha_error = 0
+    while recapcha_error < 1 do
+      # http://3qilabs.com/how_to/how-to-zoom-in-zoom-out-using-selenium-webdriver/
+      driver.navigate.to 'http://sal.receita.fazenda.gov.br/PortalSalInternet/faces/pages/calcContribuicoesCI/filiadosApos/selecionarOpcoesCalculoApos.xhtml'
+      driver.execute_script "document.body.style['-webkit-transform'] = \"scale(2.5)\";"
+      # https://snippets.aktagon.com/snippets/505-how-to-capture-screenshots-with-selenium-ruby-and-firefox
+      driver.save_screenshot('screenshot.png')
+      recapcha = recapcha(driver.save_screenshot('screenshot.png'))
+      driver.execute_script "document.body.style['-webkit-transform'] = \"scale(1)\";"
+      driver.find_element(id: "captcha_campo_resposta").send_keys recapcha.text
+      driver.find_element(id: "opcoesCalcContribuicoesCI:nome").click()
+      driver.find_element(id: "opcoesCalcContribuicoesCI:nome").send_keys '106.05052.503'
+      driver.find_element(id: "opcoesCalcContribuicoesCI:botaoConfirmar").click()
+      # error_element = driver.find_element(class_name: "erro")
+      begin
+        driver.find_element(class_name: "erro").displayed?
+      rescue
+        recapcha_error += 1
+      end
+    end
     driver.find_element(name: "formDadosCadastraisCalcContribuicoesCI:j_id38").click()
     driver.find_element(name: "informarSalariosContribuicaoDomestico:gridSelectSalarios:0:j_id38").send_keys "01/2021"
     driver.find_element(name: "informarSalariosContribuicaoDomestico:gridSelectSalarios:0:j_id40").send_keys "1.100,00"
-    driver.find_element(id: "informarSalariosContribuicaoDomestico:dataPag").click()
-    driver.find_element(id: "informarSalariosContribuicaoDomestico:dataPag").clear()
-    driver.find_element(id: "informarSalariosContribuicaoDomestico:dataPag").send_keys "08/02/2021"
+    # driver.find_element(id: "informarSalariosContribuicaoDomestico:dataPag").clear()
+    # driver.find_element(id: "informarSalariosContribuicaoDomestico:dataPag").click()
+    driver.find_element(id: "informarSalariosContribuicaoDomestico:dataPag").send_keys "11/02/2021"
+    # driver.find_element(id: "informarSalariosContribuicaoDomestico:dataPag").value="08/02/2021";;window.close();
     driver.find_element(name: "informarSalariosContribuicaoDomestico:j_id50").click()
     driver.find_element(name: "gridListSalariosCalculo:selected").click()
     driver.find_element(name: "formExibirDiscriminativoCI:j_id66").click()
+    # driver.find_element(class_name: "action-button").click()
     # driver.find_element(xpath: "//input[@type='submit' and @value='Consultar']").click()
 
     # sisad
